c5c7c068827c2f636ad9d8a4d491b5f96a5a954d
Stan Irvin-Wilmot <stan680@gmail.com>
2016-04-27 00:57:15 +0100

    tidying up brain loader (2)

--- JavaApplication1/src/Brain.java
+++ JavaApplication1/src/Brain.java
@@ -1,12 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Brain {
 
